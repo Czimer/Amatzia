8e8f03ab66e3d8a72b12e2d4898a70106f189044
czimer <czimer93@gmail.com>
2018-11-04 01:13:05 +0200

    add style to create acount page

--- Amatzia/Models/User.cs
+++ Amatzia/Models/User.cs
@@ -27,6 +27,7 @@ namespace Amatzia.Models
         public string Country { get; set; }
 
         [Display(Name = "User Name")]
+        [Required]
         public string UserName { get; set; }
 
         public string Password { get; set; }
