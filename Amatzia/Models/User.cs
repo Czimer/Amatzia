07c4b7c548ea3ee1ae6f7b71b193a22d720de48b
czimer <czimer93@gmail.com>
2018-11-03 12:13:02 +0200

    config login page as the first page add special layout  for this page

---
+++ Amatzia/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using static Amatzia.Utils.Enums;
+
+namespace Amatzia.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date Of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Country { get; set; }
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Amatzia/Utils/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Amatzia.Utils
+{
+    public class Enums
+    {
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+    }
+}
