0b3c374fede87387179460651591e8a4794ef9ba
czimer <czimer93@gmail.com>
2018-11-09 23:45:55 +0200

    bug fixes add google maps to user details

--- Amatzia/Utils/GlobalVars.cs
+++ Amatzia/Utils/GlobalVars.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 
 namespace Amatzia.Utils
