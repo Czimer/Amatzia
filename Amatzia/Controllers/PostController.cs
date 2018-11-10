dae75d8b53ba650deb4a9a96b4759e843d1687b8
Moshe Katz <moshek777@gmail.com>
2018-11-11 01:15:36 +0200

    adding comment, post and fix things in recepie bug in adding and editing

---
+++ Amatzia/Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Amatzia.Models;
+
+namespace Amatzia.Controllers
+{
+    public class PostController : Controller
+    {
+        private AmatziaEntities AmatziaDB = new AmatziaEntities();
+
+        public ActionResult Index()
+        {
+            ViewBag.Selected = "Blog";
+
+            IEnumerable<Recepie> recepiesToShow = (IEnumerable<Recepie>)TempData["Recepie"] ?? AmatziaDB.Recepies.Include("Comments").Include("User").ToList();
+
+            return View(recepiesToShow);
+        }
+
+    }
+}
